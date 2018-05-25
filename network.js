c24af735a6539fc455b746b88c8ae81e85c44571
Krish <codesorter2015@gmail.com>
2018-05-25 16:07:56 +0530

    Update network.js

--- network.js
+++ network.js
@@ -46,3 +46,17 @@ function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
 getUserIP(function(ip){
     alert("Got IP! :" + ip);
 });
+
+https://api.ipify.org/
+https://ipinfo.io
+https://l2.io/
+
+window.onload = function () {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "https://api.ipify.org?format=jsonp&callback=DisplayIP";
+        document.getElementsByTagName("head")[0].appendChild(script);
+    };
+    function DisplayIP(response) {
+        document.getElementById("ipaddress").innerHTML = "Your IP Address is " + response.ip;
+    } 
